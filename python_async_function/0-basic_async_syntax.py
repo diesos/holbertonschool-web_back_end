8c44dbf20ad41cd6ee9bfd1acf3ceb0b40f90328
Diesos <ozturk@live.Fr>
2024-04-26 15:08:00 +0200

    Add basic_async_syntax.py with asynchronous coroutine that takes in an integer argument

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""asynchronous coroutine that takes in an integer argument"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait up to max_delay seconds and then return length of delay"""
+    delay = max_delay * random.random()
+    await asyncio.sleep(delay)
+    return delay
