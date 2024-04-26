e33ef94d7426f6821a01bbc8fa46f1d838d41bb3
Diesos <ozturk@live.Fr>
2024-04-26 14:21:58 +0200

    Add to_kv function with type annotations in python_variable_annotations/7-to_kv.py

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" complex types string and int/float to tuple """
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """return tuple of string and float"""
+    return (k, v**2)
