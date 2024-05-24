4d81d752f5aeb16d603ccea6e91b5d8f3db8530d
Diesos <ozturk@live.Fr>
2024-05-24 23:47:57 +0200

    refactor: Improve error handling in getFullResponseFromAPI function

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,16 +1,11 @@
-// Export the function to be used in other modules.
 export default function getFullResponseFromAPI(success) {
-  // Return a new promise.
   return new Promise((resolve, reject) => {
-    // Check if the "success" argument is true.
     if (success) {
-      // Resolve the promise with a success object.
       resolve({
         status: 200,
         body: "Success",
       });
     } else {
-      // Reject the promise with an error object.
       reject(new Error("The fake API is not working currently"));
     }
   });
