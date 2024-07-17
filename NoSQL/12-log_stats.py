f442fe930720ed17f4c6ba1b48083dcbe00b9e73
Diesos <ozturk@live.Fr>
2024-07-17 12:28:06 +0200

    refactor: Improve error handling in getFullResponseFromAPI function

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,33 +1,23 @@
 #!/usr/bin/env python3
-""" 12-log_stats """
+""" provides some stats about Nginx logs stored in MongoDB """
+
+
 from pymongo import MongoClient
 
 
-def log_stats():
-    """Provides some stats about Nginx logs stored in MongoDB"""
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    db = client.logs
-    collection = db.nginx
-
-    # Counting total logs
-    total_logs = collection.count_documents({})
-
-    # Counting methods
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {
-        method: collection.count_documents({"method": method}) for method in methods
-    }
-
-    # Counting status check
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-
-    # Displaying stats
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{status_check} status check")
-
-
 if __name__ == "__main__":
-    log_stats()
+    """ provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    count = collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print(f"{count} logs")
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    stats = collection.count_documents({"method": "GET", "path": "/status"})
+
+    print(f"{stats} status check")
