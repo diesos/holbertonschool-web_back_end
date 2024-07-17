f70669dd3fc901dcfc87f4cb65dbb4b1da7c27f6
Diesos <ozturk@live.Fr>
2024-07-17 12:22:34 +0200

    refactor: Improve error handling in getFullResponseFromAPI function

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
-""" Script that provides stats about Nginx logs stored in MongoDB """
-
+"""
+provides some stats about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """ Script that provides stats about Nginx logs stored in MongoDB """
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    # get datatabase
+    collection = client.logs.nginx
 
-    database = client.logs
-    collection = database.nginx
+    print(f"{collection.count_documents({})} logs")
 
-    count_logs = collection.count_documents({})
-
-    all_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    print(f"{count_logs} logs")
     print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    for method in all_methods:
-        count_methods = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count_methods}")
+    number_of_gets = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{number_of_gets} status check")
 
-    result = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{result} status check")
+    print("IPs:")
+    sorted_ips = collection.aggregate(
+        [{"$group": {"_id": "$ip", "count": {"$sum": 1}}}, {"$sort": {"count": -1}}]
+    )
+    i = 0
+    for s in sorted_ips:
+        if i == 10:
+            break
+        print(f"\t{s.get('_id')}: {s.get('count')}")
+        i += 1
